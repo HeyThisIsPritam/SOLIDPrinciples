73da472b9a4638a3af32ce86acc56098e98111c7
Pritam Biswas <prit.biswas1996@gmail.com>
2022-05-21 11:35:57 +0530

    Pushing DIP

---
+++ SolidPrinciples/D - SOLID/BusinessLogicLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.D___SOLID
+{
+    public class BusinessLogicLayer // BusinessLogicLayer is a higher level module . And it not directly dependent on lower level module 
+    {                               // as it has referance to a Interface which is inplemented by lower level module,Thus achived DIP 
+        private readonly IRepository DAL;
+        public BusinessLogicLayer(IRepository repository)
+        {
+            DAL = repository;
+        }
+        public void Save(Object details)
+        {
+            DAL.save(details);
+            //Some more code.
+        }
+
+    }
+}
---
+++ SolidPrinciples/D - SOLID/DataAccessLayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.D___SOLID
+{
+    public class DataAccessLayer : IRepository // DataAccessLAyer is a lower level module
+    {
+        public void save(Object details)
+        {
+            // Some code for saving.
+        }
+    }
+}
